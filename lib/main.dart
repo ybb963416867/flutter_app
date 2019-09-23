c31062955b8dd7939b30d831abd84ab9f88f64ac
yangbinbing <yangbinbing@hachismart.com>
2019-09-23 14:11:41 +0800

    md2

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,25 @@ class MyScaffold extends StatelessWidget {
             ),
           ),
           new Expanded(
-            child: new Counter(),
+            child: new Container(
+              constraints: new BoxConstraints.expand(
+                height:Theme.of(context).textTheme.display1.fontSize * 1.1 + 200.0,
+              ),
+              decoration: new BoxDecoration(
+                border: new Border.all(width: 2.0, color: Colors.red),
+                color: Colors.grey,
+                borderRadius: new BorderRadius.all(new Radius.circular(20.0)),
+                image: new DecorationImage(
+                  image: new NetworkImage('http://h.hiphotos.baidu.com/zhidao/wh%3D450%2C600/sign=0d023672312ac65c67506e77cec29e27/9f2f070828381f30dea167bbad014c086e06f06c.jpg'),
+                  centerSlice: new Rect.fromLTRB(270.0, 180.0, 1360.0, 730.0),
+                ),
+              ),
+              padding: const EdgeInsets.all(28.0),
+              alignment: Alignment.center,
+              child: new Text('Hello World',
+                  style: Theme.of(context).textTheme.display1.copyWith(color: Colors.black)),
+              transform: new Matrix4.rotationZ(0),
+            ),
           )
         ],
       ),
@@ -87,11 +105,13 @@ class _CounterState extends State<Counter> {
   return new Row(
     children: <Widget>[
       new  CounterIncrement(onPressed: _increment,),
-      new  CounterDisplay(count: _counter,)
+      new  CounterDisplay(count: _counter,),
     ],
   );
   }
 }
+
+
 
 class MyButton extends StatelessWidget {
   @override
